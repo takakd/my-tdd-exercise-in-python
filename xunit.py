9db5836a2afb8afd4a4e62260ca14902d5e01b88
takakd <takakd@takakd.com>
2020-05-17 01:59:16 +0900

    green

--- xunit.py
+++ xunit.py
@@ -1,17 +1,25 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        super().__init__(name)
+
     def testMethod(self):
         self.wasRun = True
 
 
-test = WasRun("testMethod")
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
 
-print(test.wasRun)
-
-test.testMethod()
-
-print(test.wasRun)
-
-
-
+TestCaseTest("testRunning").run()
