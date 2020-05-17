ac7acabe13b6f2584741b64f5607fd13285c59f7
takakd <takakd@takakd.com>
2020-05-17 12:45:43 +0900

    green

--- xunit.py
+++ xunit.py
@@ -17,10 +17,8 @@ class WasRun(TestCase):
 
 # Test
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert("setUp testMethod " == self.test.log)
 
