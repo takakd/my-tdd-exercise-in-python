7824909cc6fcb89485de4e9d6903b8c14e2f4504
takakd <takakd@takakd.com>
2020-05-17 09:42:14 +0900

    green

--- xunit.py
+++ xunit.py
@@ -1,8 +1,10 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -10,7 +12,8 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
-
+    def setUp(self):
+        self.wasSetUp = True
     def testMethod(self):
         self.wasRun = True
 
