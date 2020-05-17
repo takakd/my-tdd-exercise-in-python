5f5af35f7bccc5ea68cb195e9f14f2901bf48aca
takakd <takakd@takakd.com>
2020-05-17 13:00:16 +0900

    green

--- xunit.py
+++ xunit.py
@@ -21,8 +21,10 @@ class WasRun(TestCase):
         self.log = self.log + "tearDown "
 
 class TestResult:
+    def __init__(self):
+        self.runCount = 1
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runCount
 
 # -------------------------------------
 # Test
