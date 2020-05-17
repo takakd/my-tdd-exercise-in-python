40706fc2d16d22091e8f4f39cfbdaecc7c48731b
takakd <takakd@takakd.com>
2020-05-17 12:51:01 +0900

    red

--- xunit.py
+++ xunit.py
@@ -3,16 +3,21 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = "tearDown "
 
 
 # Test
@@ -20,6 +25,6 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod tearDown" == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
