8648c7b958ae83dee0f38b2a7d99c60d608d6a85
takakd <takakd@takakd.com>
2020-05-17 09:35:29 +0900

    red

--- xunit.py
+++ xunit.py
@@ -22,4 +22,10 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.wasRun)
 
+    def testSetup(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
